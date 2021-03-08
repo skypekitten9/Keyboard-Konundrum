c9ef5ae3db7361492e49af5e3d2babd1fadca26c
Filip Moreau <filip.moreau@hotmail.com>
2021-03-08 22:29:24 +0100

    Implemented correct tile physics. Toggle & Timer interaction types implemented

--- Assets/Scripts/Action.cs
+++ Assets/Scripts/Action.cs
@@ -1,23 +1,42 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+
+[RequireComponent(typeof(Rigidbody))]
 public class Action : MonoBehaviour
 {
-    public bool activated { get; set; } = false;
-    public float speed;
-    float value;
-    [SerializeField] private Vector2 scaleClamp;
-    void Update()
+    public bool Activated { get; set; } = false;
+
+    [SerializeField] private float speed;
+    [SerializeField] private Vector2 positionClamp;
+
+    private float value;
+    private Vector3 startPosition;
+    private Rigidbody rb;
+
+
+
+    private void Awake()
     {
-        if (activated)
+        startPosition = gameObject.transform.position;
+        value = startPosition.y;
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    private void Update()
+    {
+        if (Activated)
         {
-            value = Mathf.Clamp(value += Time.deltaTime * speed, scaleClamp.x, scaleClamp.y);
+            value = Mathf.Clamp(value += Time.deltaTime * speed, positionClamp.x, positionClamp.y);
         }
         else
         {
-            value = Mathf.Clamp(value -= Time.deltaTime * speed, scaleClamp.x, scaleClamp.y);
+            value = Mathf.Clamp(value -= Time.deltaTime * speed, positionClamp.x, positionClamp.y);
         }
-        gameObject.transform.localScale = new Vector3(gameObject.transform.localScale.x, value, gameObject.transform.localScale.z);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(new Vector3(startPosition.x, value, startPosition.z));
     }
 }
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,41 +1,64 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Diagnostics;
 using UnityEngine;
 using static Enums;
 
+[RequireComponent(typeof(Action))]
 public class Tile : MonoBehaviour
 {
-    public KeyCode key;
-    public InteractionType interactionType;
-    Action action;
+    [SerializeField] private KeyCode key;
+    [SerializeField] private InteractionType interactionType;
+    private Action action;
 
-    private void Start()
+    private Stopwatch stopwatch;
+
+
+    private void Awake()
     {
         action = gameObject.GetComponent<Action>();
+        stopwatch = new Stopwatch();
     }
-    void Update()
+
+    private void Update()
     {
         switch (interactionType)
         {
             case InteractionType.Hold:
-                action.activated = Input.GetKey(key);
-                //if (Input.GetKey(key) && !action.activated)
-                //{
-                //    action.Activate();
-                //}
-                //else if (!Input.GetKey(key) && action.activated)
-                //{
-                //    action.Deactivate();
-                //}
+                {
+                    action.Activated = Input.GetKey(key);
+                }
                 break;
+
             case InteractionType.Toggle:
+                {
+                    if (Input.GetKeyDown(key))
+                    {
+                        action.Activated = !action.Activated;
+                    }
+                }
                 break;
+
             case InteractionType.Timer:
+                {
+                    if (Input.GetKeyDown(key))
+                    {
+                        stopwatch.Restart();
+                        action.Activated = true;
+                    }
+
+                    if (stopwatch.ElapsedMilliseconds >= 3000.0f)
+                    {
+                        action.Activated = false;
+                        stopwatch.Stop();
+                    }
+                }
                 break;
+
             case InteractionType.Tap:
-                break;
-            default:
+                {
+
+                }
                 break;
         }
     }
+
 }
