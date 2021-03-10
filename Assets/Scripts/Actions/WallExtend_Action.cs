5ead021ea2d590c83159690551d30a9f41c44a7d
Filip Moreau <filip.moreau@hotmail.com>
2021-03-10 22:51:51 +0100

    Refactored Action system. Action components should now derive from Action

---
+++ Assets/Scripts/Actions/WallExtend_Action.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class WallExtend_Action : Action
+{
+    private Rigidbody rb;
+    private Vector3 startPosition;
+
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+
+        rb = GetComponent<Rigidbody>();
+        startPosition = transform.position;
+    }
+
+    protected override void UpdateAction()
+    {
+        rb.MovePosition(new Vector3(startPosition.x, startPosition.y + value, startPosition.z));
+    }
+}
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,64 +1,8 @@
-﻿using System.Diagnostics;
-using UnityEngine;
-using static Enums;
+﻿using UnityEngine;
 
-[RequireComponent(typeof(Action))]
+
 public class Tile : MonoBehaviour
 {
     [SerializeField] private KeyCode key;
-    [SerializeField] private InteractionType interactionType;
-    private Action action;
-
-    private Stopwatch stopwatch;
-
-
-    private void Awake()
-    {
-        action = gameObject.GetComponent<Action>();
-        stopwatch = new Stopwatch();
-    }
-
-    private void Update()
-    {
-        switch (interactionType)
-        {
-            case InteractionType.Hold:
-                {
-                    action.Activated = Input.GetKey(key);
-                }
-                break;
-
-            case InteractionType.Toggle:
-                {
-                    if (Input.GetKeyDown(key))
-                    {
-                        action.Activated = !action.Activated;
-                    }
-                }
-                break;
-
-            case InteractionType.Timer:
-                {
-                    if (Input.GetKeyDown(key))
-                    {
-                        stopwatch.Restart();
-                        action.Activated = true;
-                    }
-
-                    if (stopwatch.ElapsedMilliseconds >= 3000.0f)
-                    {
-                        action.Activated = false;
-                        stopwatch.Stop();
-                    }
-                }
-                break;
-
-            case InteractionType.Tap:
-                {
-
-                }
-                break;
-        }
-    }
-
+    public KeyCode Key { get { return key; } }
 }
