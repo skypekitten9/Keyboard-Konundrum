3ea687a338dd31ba6e53c40bdddd30e59ef93e35
skypekitten9 <victor.enhorning@gmail.com>
2021-03-10 22:28:15 +0100

    Added copy joint script.

---
+++ Assets/Scripts/CopyJoint.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyJoint : MonoBehaviour
+{
+    [SerializeField] private Transform targetLimb;
+    private CharacterJoint characterJoint;
+    Quaternion targetInitialRotation;
+
+    //void Start()
+    //{
+    //    this.characterJoint = this.GetComponent<CharacterJoint>();
+    //    this.targetInitialRotation = this.targetLimb.transform.localRotation;
+    //}
+
+    //private void FixedUpdate()
+    //{
+    //    characterJoint.targetRotation = copyRotation();
+    //}
+
+    //private Quaternion copyRotation()
+    //{
+    //    return Quaternion.Inverse(this.targetLimb.localRotation) * this.targetInitialRotation;
+    //}
+}
