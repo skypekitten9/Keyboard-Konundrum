e471426087ab86e7317632967b691afa378ffcd3
skypekitten9 <victor.enhorning@gmail.com>
2021-03-11 10:21:35 +0100

    Ragdolls now simulates animation controller.

--- Assets/Scripts/CopyJoint.cs
+++ Assets/Scripts/CopyJoint.cs
@@ -5,22 +5,30 @@ using UnityEngine;
 public class CopyJoint : MonoBehaviour
 {
     [SerializeField] private Transform targetLimb;
-    private CharacterJoint characterJoint;
+    private ConfigurableJoint characterJoint;
+    public bool enabled;
     Quaternion targetInitialRotation;
 
-    //void Start()
-    //{
-    //    this.characterJoint = this.GetComponent<CharacterJoint>();
-    //    this.targetInitialRotation = this.targetLimb.transform.localRotation;
-    //}
+    void Start()
+    {
+        if(enabled)
+        {
+            this.characterJoint = this.GetComponent<ConfigurableJoint>();
+            this.targetInitialRotation = this.targetLimb.transform.localRotation;
+        }
+        
+    }
 
-    //private void FixedUpdate()
-    //{
-    //    characterJoint.targetRotation = copyRotation();
-    //}
+    private void FixedUpdate()
+    {
+        if (enabled)
+        {
+            characterJoint.targetRotation = copyRotation();
+        }
+    }
 
-    //private Quaternion copyRotation()
-    //{
-    //    return Quaternion.Inverse(this.targetLimb.localRotation) * this.targetInitialRotation;
-    //}
+    private Quaternion copyRotation()
+    {
+        return Quaternion.Inverse(this.targetLimb.localRotation) * this.targetInitialRotation;
+    }
 }
