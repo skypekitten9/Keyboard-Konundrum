e66a98d02ec696bd15eb1b10ee9e4f810922c546
skypekitten9 <victor.enhorning@gmail.com>
2021-03-14 16:36:33 +0100

    Added a goal model and wrote a basic script for it.

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    bool gameOver = false;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.tag == "Player" && !gameOver)
+        {
+            transform.GetComponent<Rigidbody>().isKinematic = false;
+            gameOver = true;
+            Win();
+        }
+    }
+
+    private void Win()
+    {
+        Debug.Log("You just won the game!");
+    }
+}
