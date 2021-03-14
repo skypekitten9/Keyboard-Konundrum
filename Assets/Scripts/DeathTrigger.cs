69aa9fd5aad92c931821ae3f9537b5dc71d35690
Filip Moreau <filip.moreau@hotmail.com>
2021-03-14 19:32:05 +0100

    Simple death trigger created. Reloads scene upon "death"

---
+++ Assets/Scripts/DeathTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Collider))]
+public class DeathTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Death triggered - reloading scene...");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
