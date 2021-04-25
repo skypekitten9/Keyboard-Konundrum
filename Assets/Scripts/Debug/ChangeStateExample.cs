4d3ec20da2c589bc9f47f74d2f15723e207373fe
skypekitten9 <victor.enhorning@gmail.com>
2021-04-25 21:31:07 +0200

    Re-organized assets, removed old animations and imported new ones to test state transition on ragdoll.

---
+++ Assets/Scripts/Debug/ChangeStateExample.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using RagdollMecanimMixer;
+using UnityEngine;
+public class ChangeStateExample : MonoBehaviour
+{
+    private RamecanMixer ramecanMixer;
+    void Start()
+    {
+        ramecanMixer = GetComponent<RamecanMixer>();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            ramecanMixer.BeginStateTransition("default");
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+            ramecanMixer.BeginStateTransition("dead");
+    }
+}
