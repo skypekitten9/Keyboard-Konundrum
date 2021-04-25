500e9a7216e8afdab50531250fc58dfbb371b283
skypekitten9 <victor.enhorning@gmail.com>
2021-04-25 21:46:05 +0200

    Ragdoll created.

--- Assets/Scripts/Debug/ChangeStateExample.cs
+++ Assets/Scripts/Debug/ChangeStateExample.cs
@@ -5,15 +5,26 @@ using UnityEngine;
 public class ChangeStateExample : MonoBehaviour
 {
     private RamecanMixer ramecanMixer;
+    private Rigidbody rb;
     void Start()
     {
         ramecanMixer = GetComponent<RamecanMixer>();
+        rb = GetComponent<Rigidbody>();
     }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Vector3 revivePos = ramecanMixer.RootBoneTr.position;
+            rb.position = new Vector3(revivePos.x, rb.position.y, revivePos.z);
             ramecanMixer.BeginStateTransition("default");
+        }
+            
         if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
             ramecanMixer.BeginStateTransition("dead");
+        }
+
+      
     }
 }
