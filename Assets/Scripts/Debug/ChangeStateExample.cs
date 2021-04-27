9971d1bccbc3ca0bf1d1578d3b7da9b5426330e3
skypekitten9 <victor.enhorning@gmail.com>
2021-04-27 17:03:07 +0200

    Added scripts to test movement & collision of ragdoll.

--- Assets/Scripts/Debug/ChangeStateExample.cs
+++ Assets/Scripts/Debug/ChangeStateExample.cs
@@ -6,25 +6,37 @@ public class ChangeStateExample : MonoBehaviour
 {
     private RamecanMixer ramecanMixer;
     private Rigidbody rb;
+    private bool dead;
+
     void Start()
     {
         ramecanMixer = GetComponent<RamecanMixer>();
         rb = GetComponent<Rigidbody>();
+        dead = false;
     }
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            ramecanMixer.BeginStateTransition("default");
+            dead = false;
+            rb.isKinematic = false;
+        }
+            
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            ramecanMixer.BeginStateTransition("dead");
+            dead = true;
+            rb.isKinematic = true;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if(dead)
         {
             Vector3 revivePos = ramecanMixer.RootBoneTr.position;
             rb.position = new Vector3(revivePos.x, rb.position.y, revivePos.z);
-            ramecanMixer.BeginStateTransition("default");
         }
-            
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            ramecanMixer.BeginStateTransition("dead");
-        }
-
-      
     }
 }
