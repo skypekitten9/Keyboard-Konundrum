18650fecf3a48ab5e188399d159d9578d6fcb989
skypekitten9 <victor.enhorning@gmail.com>
2021-06-06 13:34:13 +0200

    Setup controller to test input.

---
+++ Assets/Input/InputDebugController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputDebugController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnDebug(InputAction.CallbackContext value)
+    {
+        Debug.Log("Button pressed!");
+    }
+}
